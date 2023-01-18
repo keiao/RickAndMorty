6cf6f4819abd05411a7dbddc9cc28d20f389d102
Marlon Cardozo <Marlon@MacBook-Pro-de-Mac.local>
2023-01-18 10:30:56 -0500

    first commit

---
+++ src/Home.vue
@@ -0,0 +1,111 @@
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+    name: "App",
+    components: HelloWorld,
+    components: { HelloWorld }
+}
+</script>
+
+<template>
+  <div class="home">
+    <img src="../public/rick and morty.svg" class="logo" alt="logoRM" />
+
+    <h1>Bienvenido a Rick and Morty</h1>
+
+    <p>
+      En esta prueba, evaluaremos su capacidad para construir la aplicación
+      mediante el análisis de código y la reproducción del siguiente diseño.
+    </p>
+
+
+    <button>
+      <a href="./components/HelloWorld.vue" class="button">Continuar</a>
+    </button>
+    
+  </div>
+
+  <img src="../public/Welcome.svg" class="svg" alt="rickAndMorty" />
+  <HelloWorld />
+</template>
+
+<style>
+.home {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  margin: 200px 0 0 ;
+  padding: 0;
+}
+
+.logo {
+  z-index: 1;
+}
+
+.svg {
+  position: fixed;
+  left: 0px;
+  top: -100px;
+  z-index: -1;
+}
+
+h1 {
+  color: var(--one-color);
+  font-weight: 300;
+  font-size: 36px;
+}
+
+p {
+  max-width: 50%;
+  text-align: center;
+  color:  var(--one-color);
+  font-weight: 200;
+  font-size: 18px;
+  padding-bottom: 15px;
+}
+
+
+button {
+  background: var(--two-color);
+  border: none; 
+  border-radius: 60px; 
+  position: relative;
+  z-index: 1;
+  overflow: hidden;
+  display: inline-block;
+  width: 150px;
+  height: 50px;
+}
+
+button a {
+  color:  var(--one-color);
+  font-weight: 300px;
+  font-size: 18px;
+  
+}
+button:hover a{
+  color:  var(--two-color);
+}
+button:after {
+  content: "";
+  background: var(--three-color); 
+  position: absolute;
+  z-index: -1;
+  padding: 16px 20px;
+  display: block;
+  border-radius: 50%;
+  left: -50%;
+  right: -50%;
+  top: -150%;
+  bottom: -150%;
+  transform: scale(0, 0);
+  transition: all 0.4s ease;
+}
+button:hover::after {
+  transition: all 0.6s ease-out;
+  transform: scale(1, 1);
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue'
+import Home from '../Home.vue'
+
+</script>
+
+<template>
+<header>
+<img src="../../public/Welcome.svg" alt="">
+</header>
+</template>
+
+<style scoped>
+</style>
